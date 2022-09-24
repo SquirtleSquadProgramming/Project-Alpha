ad895250ed70f1f2a23e7946a7761ae3dd551065
nathan <nathan.titirangi@gmail.com>
2022-09-24 20:54:56 +1200

    functiong movement

--- Project Alpha/Assets/Scripts/PlayerMove.cs
+++ Project Alpha/Assets/Scripts/PlayerMove.cs
@@ -36,19 +36,22 @@ public class PlayerMove : MonoBehaviour
     void MovePlayer()
     {
         Vector3 currentVelocity = rb.velocity;
-        float theta = player.transform.rotation.eulerAngles.y;
+        float theta = player.transform.rotation.eulerAngles.y * (float)Math.PI / 180f;
         Vector3 rotatedVelocity = new Vector3(
             currentVelocity.x * (float)Math.Sin(theta) + currentVelocity.y * (float)Math.Cos(theta),
             currentVelocity.x * (float)Math.Cos(theta) + currentVelocity.y * (float)Math.Sin(theta),
             currentVelocity.z);
-        Debug.Log(rotatedVelocity);
-        rb.AddForce(speed * new Vector3(prevInputs.x * (float)Math.Sin(theta) + prevInputs.y * (float)Math.Cos(theta),
-        prevInputs.x * (float)Math.Sin(theta) + prevInputs.y * (float)Math.Cos(theta)), ForceMode.Force);
+        
+        rb.AddForce(
+            speed * new Vector3(
+                prevInputs.x * (float)Math.Cos(theta) + prevInputs.y * (float)Math.Sin(theta),
+                0f,
+                prevInputs.x * (float)Math.Sin(theta) + prevInputs.y * (float)Math.Cos(theta)), ForceMode.Force);
         prevInputs = new Vector2(0f, 0f);
     }
     void FixedUpdate()
     {
-        // MovePlayer();
+        MovePlayer();
     }
     void Update()
     {
