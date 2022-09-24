1373e8a8d34d3dc35845d10410511178fd4f5c92
nathan <nathan.titirangi@gmail.com>
2022-09-24 16:53:24 +1200

    I am going rn

--- Project Alpha/Assets/Scripts/PlayerMove.cs
+++ Project Alpha/Assets/Scripts/PlayerMove.cs
@@ -4,6 +4,7 @@ public class PlayerMove : MonoBehaviour
 {
     GameObject player;
     Camera playerCamera;
+    GameObject body;
     public (float,float) sensitivity = (1f,1f); //X,Y
     // Start is called before the first frame update
     void Start()
@@ -12,6 +13,10 @@ public class PlayerMove : MonoBehaviour
         foreach (Transform child in player.transform)
         {
             if (child.gameObject.GetComponent<Camera>() != null)
+            {
+                playerCamera = child.gameObject.GetComponent<Camera>();
+            }
+            if (child.gameObject.tag == "playerBody")
             {
                 playerCamera = child.gameObject.GetComponent<Camera>();
             }
@@ -24,6 +29,10 @@ public class PlayerMove : MonoBehaviour
         Vector3 angles = playerCamera.transform.rotation.eulerAngles;
         Debug.Log(Input.GetAxis("Mouse X"));
         player.transform.rotation = Quaternion.Euler(angles + new Vector3(sensitivity.Item2 * Input.GetAxis("Mouse Y") * -1f, sensitivity.Item1 * Input.GetAxis("Mouse X"),0f));
+    }
+    void MovePlayer()
+    {
+        Vector3 currentVelocity = body.GetComponent<Rigidbody>().velocity;
     }
     void Update()
     {
