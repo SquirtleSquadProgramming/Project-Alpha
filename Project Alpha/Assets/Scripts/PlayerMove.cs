625198d0d7fb414c4d86ad8788ccac5b0fd82d7c
nathan <nathan.titirangi@gmail.com>
2022-09-25 11:56:13 +1300

    movement is hard

--- Project Alpha/Assets/Scripts/PlayerMove.cs
+++ Project Alpha/Assets/Scripts/PlayerMove.cs
@@ -7,13 +7,15 @@ public class PlayerMove : MonoBehaviour
     GameObject player;
     Rigidbody rb;
     public Camera playerCamera;
-    public float speed = 1000.0f;
-    public float maxVel = 1.0f;
+    public float speed = 5000.0f;
+    public float velocityScaling = -0.8f; //coef1
+    public float maxVel = 0.3f; //coef2
     Vector2 prevInputs; //make movement feel not garbage
 
     // Start is called before the first frame update
     void Start()
     {
+        PlayerData.MouseSensitivity = new Vector2(100f,100f);
         player = gameObject;
         rb = player.GetComponent<Rigidbody>();
     }
@@ -33,6 +35,10 @@ public class PlayerMove : MonoBehaviour
             )
         );
     }
+    float VelocityScale(float directional)
+    {
+        return 1-(Math.Min((1-1/(1+Math.Abs(directional*velocityScaling))+maxVel),1)-maxVel)*(1/(1-maxVel)); //https://www.desmos.com/calculator/srjtyzbiiv
+    }
 
     void MovePlayer()
     {
@@ -42,14 +48,27 @@ public class PlayerMove : MonoBehaviour
         represents the player relative left-right forward-backwards velocity multipliers
         they are calculated as velocity by (min((1-1/(1+abs(velocityInDirection*coef1))+coef2),1)-coef2)*(1/(1-coef2))
         */
-        Vector3 rotatedVelocity = Quaternion.Euler(new Vector3(0f,theta,0f)) * currentVelocity;
-        Vector3 velocityMultipliers = new Vector3();
+        // Debug.Log(Time.time*10);
+        // Debug.Log(Quaternion.Euler(new Vector3(0f,Time.time*10,0f)) * new Vector3(1f,0f,0f));
+        Vector3 rotatedVelocity = Quaternion.Euler(0f,-theta * 180 / (float)Math.PI,0f) * currentVelocity;
+        Vector2 velocityMultipliers = new Vector2(
+            VelocityScale(rotatedVelocity.x),
+            VelocityScale(rotatedVelocity.z)
+        );
 
+        Vector3 move = speed * new Vector3(
+            prevInputs.x * (float)Math.Cos(-theta) * velocityMultipliers.x + prevInputs.y * (float)Math.Sin(theta) * velocityMultipliers.y,
+            0f,
+            prevInputs.x * (float)Math.Sin(-theta) * velocityMultipliers.x + prevInputs.y * (float)Math.Cos(theta) * velocityMultipliers.y
+        );
+        // Debug.Log(rotatedVelocity);
+        // Debug.Log("Normal:  " + currentVelocity.ToString());
+        // Debug.Log("Rotated: " + rotatedVelocity.ToString());
+        Debug.Log("Mul: " + velocityMultipliers.ToString());
+        Debug.Log("In:  " +  prevInputs.ToString());
         rb.AddForce(
-            speed * new Vector3(
-                prevInputs.x * (float)Math.Cos(-theta) + prevInputs.y * (float)Math.Sin(theta),
-                0f,
-                prevInputs.x * (float)Math.Sin(-theta) + prevInputs.y * (float)Math.Cos(theta)), ForceMode.Force);
+            move
+            , ForceMode.Force);
         prevInputs = new Vector2(0f, 0f);
     }
 
